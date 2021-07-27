80c64ae1c8a65cd9faecee08328e6e23ac1d157a
Ivan <vanuha277@gmail.com>
2021-07-27 12:31:35 +0300

    refactoring and did more useable

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,7 +3,8 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['source'];
 
-  copy() {
+  copy(event) {
+    event.preventDefault(); // For removing default url inside 'href', like '#'
     this.sourceTarget.select();
     document.execCommand('copy');
   }
